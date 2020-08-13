ffcee837fe76b1e24d9f30f07ab7c82c90abc219
christian cecilia <christian.cecilia1@gmail.com>
2020-08-13 09:02:24 -0400

    addde callback to user

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('compiler test')
+import { User } from "./models/User";
+
+const user = new User({name: 'chris', age: 30});
+
+user.set({name: 'christian'});
+
+console.log(user.get('name'));
+console.log(user.get('age'));
